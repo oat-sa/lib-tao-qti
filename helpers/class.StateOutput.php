380af26f79c4c0499f1c02bc9a51ab44f5fe270d
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-09-16 11:50:50 +0000

    EndPoints for QTI Tests Runners now respond with the item session state.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiCommon/trunk@16170 62dfeb86-a93f-4914-9d13-15c0a160985d

--- helpers/class.StateOutput.php
+++ helpers/class.StateOutput.php
@@ -64,9 +64,47 @@ class taoQtiCommon_helpers_StateOutput {
     }
     
     /**
-     * Get the output array.
+     * Get the output array. The returned array is an associative array where keys are the identifier
+     * of the variable and the values are the actual values of the variable.
      * 
-     * @return array
+     * Each variable value is represented as an array, even if the cardinality of the variable is SINGLE. The values
+     * are always plain string, as the values coming in from the client-side. Find below an example of returned state
+     * output. Please be carefull will null values. They will be transformed as empty strings.
+     * 
+     * <code>
+     * // The  $stateOutput variable is a StateOutput object where the following variables
+     * // where added through the StateOutput::addVariable() method.
+     * // - OutcomeVariable => 'SCORE1', Cardinality:SINGLE, BaseType:INTEGER, value: 25
+     * // - OutcomeVariable => 'SCORE2', Cardinality:SINGLE, BaseType:URI, value: 'http://bit.ly'
+     * // - OutcomeVariable => 'SCORE3', Cardinality:SINGLE, BaseType:BOOLEAN, value: false
+     * // - OutcomeVariable => 'SCORE4', Cardinality:SINGLE, BaseType:FLOAT, value: null
+     * // - OutcomeVariable => 'SCORE5', Cardinality::ORDERED, BaseType:INTEGER, value: {0, -10, null, 44}
+     * // - OutcomeVariable => 'SCORE6', Cardinality::MULTIPLE, BaseType:PAIR, value: [new Pair('A', 'B'), new Pair('C', 'D')]
+     * // - OutcomeVariable => 'SCORE7', Cardinality::MULTIPLE, BaseType:POINT, value: [new Point(0, 0), new Point(-3, 4)]
+     * // - OutcomeVariable => 'SCORE8', Cardinality::RECORD, value: <'val1': 25.3, 'val2': null, 'val3': new Duration('PT1S')>
+     * $stateOutput = new taoQtiCommon_helpers_StateOutput();
+     * // ...
+     * // $stateOutput->addVariable() ...
+     * // $stateOutput->addVariable() ...
+     * // $stateOutput->addVariable() ...
+     * // ..
+     * 
+     * $output = $stateOutput->getOutput();
+     * 
+     * // The content of $output is:
+     * // array(
+     * //     'SCORE1' => array('25'),
+     * //     'SCORE2' => array('http://bit.ly'),
+     * //     'SCORE3' => array('false'),
+     * //     'SCORE4' => array(''),
+     * //     'SCORE5' => array('0', '-10', '', '44'),
+     * //     'SCORE6' => array(array('A', 'B'), array('C', 'D')),
+     * //     'SCORE7' => array(array('0', '0'), array('-3', '4'))
+     * //     'SCORE8' => array('val1' => '25.3', 'val2' => '', 'val3' => 'PT1S')
+     * // );
+     * </code>
+     * 
+     * @return array An array of QTI Variables and their values formatted as explained supra.
      */
     public function &getOutput() {
         return $this->output;
