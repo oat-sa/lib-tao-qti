54d94339a05412b00f74038ddf79fdd8596887d2
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-03-26 14:52:09 +0000

    cleanup runtime
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiCommon/trunk@19302 62dfeb86-a93f-4914-9d13-15c0a160985d

--- manifest.php
+++ manifest.php
@@ -58,20 +58,8 @@ return array(
 		'BASE_PATH'				=> $extpath,
 	
 		#BASE URL (usually the domain root)
-		'BASE_URL'				=> ROOT_URL	.'taoQtiTest/',
+		'BASE_URL'				=> ROOT_URL	.'taoQtiCommon/',
 	
-		#BASE WWW the web resources path
-		'BASE_WWW'				=> ROOT_URL	.'taoQtiTest/views/',
-	
-		#BASE DATA the path where items are stored
-		'BASE_DATA'				=> $extpath.'data'.DIRECTORY_SEPARATOR,
-	
-		#BASE PREVIEW the path where items are compiled for preview
-		'BASE_PREVIEW'			=> $extpath.'views'.DIRECTORY_SEPARATOR.'runtime'.DIRECTORY_SEPARATOR,
-
-		#BASE PREVIEW URL the url pointing at where items can be previewed
-		'BASE_PREVIEW_URL'		=> ROOT_URL.'taoItems/views/runtime/',
-	 
 	 	#TAO extension Paths
 		'TAOBASE_WWW'			=> ROOT_URL	.'tao/views/',
 		'TAOVIEW_PATH'			=> $taopath	.'views'.DIRECTORY_SEPARATOR,
