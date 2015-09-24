81c3ab9264c6c4ef90bb55b090b3fad84d4bfdd1
= <jerome@taotesting.com>
2015-09-24 13:11:30 +0200

    Custom Operators, the beginning in TAO.

---
+++ model/operators/text/StringToNumber.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace qti\customOperators\text;
+
+use qtism\common\datatypes\String;
+use qtism\common\datatypes\Float;
+use qtism\runtime\expressions\operators\CustomOperatorProcessor;
+
+class StringToNumber extends CustomOperatorProcessor
+{
+    public function process() 
+    {
+        $returnValue = null;
+        
+        $operands = $this->getOperands();
+        if (count($operands) > 0) {
+            $operand = $operands[0];
+            
+            if ($operand !== null && $operand instanceof String) {
+                $str = str_replace(array(','), '', $operand->getValue());
+                $float = @floatval($str);
+                
+                $returnValue = new Float($float);
+            }
+        }
+        
+        return $returnValue;
+    }
+}
